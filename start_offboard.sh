0f6b8bf677862bcbf9734760f077ed1a1129844b
JeremyFrederick0 <124828647+JeremyFrederick0@users.noreply.github.com>
2024-08-23 13:03:49 -0700

    Update start_offboard.sh

--- start_offboard.sh
+++ start_offboard.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-if [ $# -ne 3 ]; then
+if [ $# -ne 2 ]; then
     echo "Error: Exactly three arguments are required in the following order."
-    echo "Usage: ./script_name.sh <connection port> <desired alititude in meters> <desired pattern in meters>"
-    echo "Correct Format: ./start_offboard.sh udp://:14551 1.0 2.0"
+    echo "Usage: ./script_name.sh <desired alititude in meters> <desired pattern in meters>"
+    echo "Correct Format: ./start_offboard.sh 1.0 2.0"
     exit 1
 fi
 
 cd ../../home/reachpower-dockercross/examples/offboard
 
-./build/offboard $1 $2 $3
+./build/offboard udp://:14551 $1 $2
